095fb2ff2413a805fecc958af7d77d0c0f410a1d
Abraham <abraham@Abrahams-MacBook-Pro.local>
2015-06-11 02:34:18 -0700

    added the comments box

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :links do
     member do
       put "like", to:    "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
+  root "links#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
-  root to: "links#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
